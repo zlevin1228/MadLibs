6d5a7ce98fb28bc3adfe258fb8a202623ee1f6fc
zlevin1228 <zacharylevin1228@gmail.com>
2023-12-07 13:42:18 -0500

    added js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-        
+let adjective = prompt('Enter an adjective:')
+let noun = prompt('Enter an noun:')
+let verb = prompt('Enter an verb:')
+let adverb = prompt('Enter an adverb:')
+
+let story = `Once upon a time, in a ${adjective} ${noun}, a ${verb} ${noun} ${adverb} ${verb}.`
+
+let madLibOutputDiv = document.querySelector('#madLibOutput')
+
+madLibOutputDiv.innerHTML = `<p>${story}</p>`
