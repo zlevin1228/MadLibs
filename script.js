4a7d675e6b124caa6a76e96b751e44947958685c
zlevin1228 <zacharylevin1228@gmail.com>
2023-12-19 14:16:45 -0500

    added images and fixed js grammar

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let adjective1 = prompt('Enter an adjective:')
-let noun1 = prompt('Enter an noun:')
-let verb1 = prompt('Enter an verb:')
+let noun1 = prompt('Enter a noun:')
+let verb1 = prompt('Enter a verb:')
 let adverb1 = prompt('Enter an adverb:')
 let athlete1 = prompt('Enter an NFL player:')
 let athlete2 = prompt('Enter an NHL player:')
